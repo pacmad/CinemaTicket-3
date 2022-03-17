c5efe7c98240976a091cff8a3ac88730db6088ce
Angel Raychev <angelraychev@gmail.com>
2022-03-17 21:23:52 +0200

    Category controller added whith view

---
+++ CinemaTicketWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using CinemaTicket.Infrastructure.Data;
+using CinemaTicket.Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaTicketWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
